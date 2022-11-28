33809515c9e573397c9382aaa908c15f06b9bda5
username <dlehddls1015@naver.com>
2022-11-28 21:30:13 +0900

    .

---
+++ src/main/java/com/bsntech/first/controller/DrinkMainController.java
@@ -0,0 +1,14 @@
+package com.bsntech.first.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DrinkMainController {
+
+    @GetMapping("/main")
+    public String get() {
+        return "";
+    }
+}
